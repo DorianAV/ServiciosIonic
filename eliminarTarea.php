e02799b7a752aadcbf0da2df08eb593353f3fd17
Dorian Cinue Arce Vázquez <doriancinue26@gmail.com>
2024-04-14 11:23:41 -0600

    1

---
+++ eliminarTarea.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+// Resto del código PHP
+
+$server = "localhost";
+$username = "root";
+$password = "2004";
+$database = "ionic";
+
+
+$conn = new mysqli($server , $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("La conexión ha fallado: " . $conn->connect_error);
+}
+
+
+$idtarea = $_GET['idtarea'];
+
+$sql = "DELETE FROM tareas WHERE idtarea = '$idtarea'";
+
+$res = mysqli_query($conn,$sql);
+echo json_encode($res);
+$conn->close();
+
+
