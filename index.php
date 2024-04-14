471b8111ceb56a45a8fcc47f1a51134f3770b11d
Dorian Cinue Arce Vázquez <doriancinue26@gmail.com>
2024-04-13 23:48:26 -0600

    1

--- index.php
+++ index.php
@@ -10,7 +10,17 @@ $idusuario = $_GET['idusuario'];
 $con = mysqli_connect('dorian-server.mysql.database.azure.com', 'sifsgliims', 'Tb59I$a7up5qLihv', 'ionic');
 
 
-$sql = "SELECT * FROM tareas where idusuario='$idusuario'";
+$sql = "create table usuarios
+(
+    idusuario int auto_increment
+        primary key,
+    correo    varchar(100) not null,
+    password  varchar(50)  not null,
+    Nombre    varchar(50)  not null,
+    constraint usuarios_pk_2
+        unique (correo)
+);
+";
 
 $result = mysqli_query($con,$sql);
 
